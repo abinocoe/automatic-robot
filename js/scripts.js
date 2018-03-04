0d7efaefd4fad457a736563713c80e383ccaa4ab
Abigail Coe <abigailcoe@gmail.com>
2018-03-04 15:19:29 +0000

    check for min no. of arguments and that first is correct format

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
-checkInput = (input) => {
-    return input;
+getResponse = (input) => {
+    if (!checkInputIsValid(input)) {
+        return "Your input is not in the correct format"
+    }
+    return true;
 }
 
-module.exports = {
-    checkInput,
-};
+checkInputIsValid = (input) => {
+    if (input) {
+        // split instructions into array
+        let instructionArray = input.trim().split(/\n/);
+        if (instructionArray.length < 3) {
+            return false;
+        }
+        checkMarsInitialise(instructionArray[0])
+    }
+    return false
+}
+
+checkMarsInitialise = (input) => {
+    let marsSize = input.split('');
+    // check that mars size arguments are 2 long and both numbers
+    if (marsSize.length === 2 && marsSize.every(checkIsInteger)) {
+        return marsSize;
+    }
+    return false;
+}
+
+checkIsInteger = (number) => {
+    return number % 1 === 0;
+}
