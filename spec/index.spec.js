686e7cf26f0bdceedd58a99d96cd12f83110f9d3
Abigail Coe <abigailcoe@gmail.com>
2018-03-04 15:14:03 +0000

    test three

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -13,3 +13,12 @@ describe('checkInputIsValid', () => {
         expect(checkInputIsValid(input)).toEqual(false)
     })
 })
+
+describe('checkMarsInitialise', () => {
+    it('should return false if argument is not 2 characters long', () => {
+        expect(checkMarsInitialise('234')).toEqual(false)
+    })
+    it('should return false if arguments are not solely integers', () => {
+        expect(checkMarsInitialise('1A')).toEqual(false)
+    })
+})
